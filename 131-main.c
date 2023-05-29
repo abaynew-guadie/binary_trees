e50a386310e9b8c9493110609ae927f6f986dbe2
abaynew-guadie <abaynew@yahoo.com>
2023-05-29 05:20:19 -0700

    task 0-41

--- 131-main.c
+++ 131-main.c
@@ -9,32 +9,33 @@
  */
 int main(void)
 {
-avl_t *root;
-avl_t *node;
+heap_t *root;
+heap_t *node;
 
 root = NULL;
-node = avl_insert(&root, 98);
+node = heap_insert(&root, 98);
 printf("Inserted: %d\n", node->n);
 binary_tree_print(root);
-node = avl_insert(&root, 402);
+node = heap_insert(&root, 402);
+
 printf("\nInserted: %d\n", node->n);
 binary_tree_print(root);
-node = avl_insert(&root, 12);
+node = heap_insert(&root, 12);
 printf("\nInserted: %d\n", node->n);
 binary_tree_print(root);
-node = avl_insert(&root, 46);
+node = heap_insert(&root, 46);
 printf("\nInserted: %d\n", node->n);
 binary_tree_print(root);
-node = avl_insert(&root, 128);
+node = heap_insert(&root, 128);
 printf("\nInserted: %d\n", node->n);
 binary_tree_print(root);
-node = avl_insert(&root, 256);
+node = heap_insert(&root, 256);
 printf("\nInserted: %d\n", node->n);
 binary_tree_print(root);
-node = avl_insert(&root, 512);
+node = heap_insert(&root, 512);
 printf("\nInserted: %d\n", node->n);
 binary_tree_print(root);
-node = avl_insert(&root, 50);
+node = heap_insert(&root, 50);
 printf("\nInserted: %d\n", node->n);
 binary_tree_print(root);
 return (0);
