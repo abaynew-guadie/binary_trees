4e1f274c9e8e162edf19d08e675bdde97574615a
abaynew-guadie <abaynew@yahoo.com>
2023-05-31 01:07:24 -0700

    task 0-41

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,58 +1,30 @@
 #include "binary_trees.h"
-
-
+#include<stdlib.h>
 /**
- * sortedArrToAVL - recursive helper to sorted_array_to_avl: creates an AVL
- * tree from an array of integers, which can be assumed to be sorted in
- * ascending order and have no repeating values
- *
- * @array: sorted array of integers
- * @lo_idx: leftmost index accessed in array
- * @hi_idx: rightmost index accessed in array
- * @parent: parent node from previous frame of recursion, or NULL for root
- * Return: pointer to new subtree's root node, or NULL on failure
+ * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of
+ * integers
+ * @heap: pointer to the root node of the heap to convert
+ * @size: address to store the size of the array
+ * Return: sorted array of integers
  */
-avl_t *sortedArrToAVL(int *array, int lo_idx, int hi_idx, avl_t *parent)
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-avl_t *root = NULL;
-int mid_idx;
+int *array;
+int extract, i = 0;
+size_t heap_size;
 
+if (!heap)
+return (NULL);
+heap_size = binary_tree_size(heap);
+*size = heap_size;
+array = malloc(heap_size * sizeof(int));
 if (!array)
 return (NULL);
-
-/* base case, cannot subdivide array any further */
-if (lo_idx > hi_idx)
-return (NULL);
-
-/* middle value becomes local root */
-/* as division rounds down, weights tree slightly to right */
-mid_idx = (lo_idx + hi_idx) / 2;
-root = binary_tree_node(parent, array[mid_idx]);
-if (!root)
-return (NULL);
-
-/* recurse to find new mid in subarray to the left of middle index */
-root->left = sortedArrToAVL(array, lo_idx, mid_idx - 1, root);
-
-/* recurse to find new mid in subarray to the right of middle index */
-root->right = sortedArrToAVL(array, mid_idx + 1, hi_idx, root);
-
-return (root);
-}
-
-
-/**
- * sorted_array_to_avl - creates an AVL tree from an array of integers sorted
- * in ascending order; array can be assumed to have no repeating values
- *
- * @array: sorted array of integers
- * @size: amount of members in array
- * Return: pointer to new tree's root node, or NULL on failure or empty array
- */
-avl_t *sorted_array_to_avl(int *array, size_t size)
+while (heap)
 {
-if (!array || size == 0)
-return (NULL);
-
-return (sortedArrToAVL(array, 0, (int)size - 1, NULL));
+extract = heap_extract(&heap);
+array[i] = extract;
+i++;
+}
+return (array);
 }
